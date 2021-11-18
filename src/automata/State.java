14c3377ae8569ea39f990e0f3c72ebfb5cc32d76
m r <a.m.rasouli.n@gmail.com>
2021-11-18 23:35:43 +0330

    Removed successors from state class, Fixed toString() method

--- src/automata/State.java
+++ src/automata/State.java
@@ -1,18 +1,14 @@
 package automata;
 
-import java.util.Dictionary;
-
 public class State {
 
     private final String name;
-    private final Dictionary successors;
     private final boolean isFinal;
     private final boolean isInitial;
 
-    public State(String name, Dictionary successors, boolean isFinal, boolean isInitial) {
+    public State(String name, boolean isFinal, boolean isInitial) {
 
         this.name = name;
-        this.successors = successors;
         this.isFinal = isFinal;
         this.isInitial = isInitial;
     }
@@ -22,10 +18,10 @@ public class State {
         String final_state = "";
         String init_state = "";
         if (this.isInitial)
-            init_state = "Initial";
+            init_state = "Initial ";
         if (this.isFinal)
-            final_state = "Final";
+            final_state = "Final ";
 
-        return String.format("%s %s State %s : %s", final_state, init_state, this.name, this.successors);
+        return String.format("%s%sState %s", final_state, init_state, this.name);
     }
 }
