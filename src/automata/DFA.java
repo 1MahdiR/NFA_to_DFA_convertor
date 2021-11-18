d0b601c6dad53615cee6975c2f6b9c80c1c12cbb
m r <a.m.rasouli.n@gmail.com>
2021-11-19 01:11:23 +0330

    Created DFA class

---
+++ src/automata/DFA.java
@@ -0,0 +1,44 @@
+package automata;
+
+import java.util.List;
+import java.util.Map;
+
+public class DFA {
+
+    private final String name;
+    private final Map<State, Map<String, State>> graph;
+    private final State initialState;
+    private final List<State> finalStates;
+
+    public DFA(String name, Map<State, Map<String, State>> graph, State initialState, List<State> finalStates) {
+        this.name = name;
+        this.graph = graph;
+        this.finalStates = finalStates;
+        this.initialState = initialState;
+    }
+
+    public boolean doesAcceptString(String w) {
+
+        State currentState = this.initialState;
+        for (int i = 0; i < w.length(); i++)
+        {
+            String target_edge = Character.toString(w.charAt(i));
+            Map<String, State> currentState_edges = this.graph.get(currentState);
+
+            if (currentState_edges.containsKey(target_edge))
+                currentState = currentState_edges.get(target_edge);
+            else
+                return false;
+        }
+
+        return this.finalStates.contains(currentState);
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format("DFA name: %s\n graph:\n %s\n initial state: %s\n final states:\n %s\n",
+                this.name, this.graph, this.initialState, this.finalStates);
+    }
+
+}
