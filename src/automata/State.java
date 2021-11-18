2ea6f89b2386198cb21141ffa42ff9e7107fbc0a
m r <a.m.rasouli.n@gmail.com>
2021-11-19 00:04:42 +0330

    Removed isFinal and isInitial from State class

--- src/automata/State.java
+++ src/automata/State.java
@@ -3,25 +3,12 @@ package automata;
 public class State {
 
     private final String name;
-    private final boolean isFinal;
-    private final boolean isInitial;
 
-    public State(String name, boolean isFinal, boolean isInitial) {
-
-        this.name = name;
-        this.isFinal = isFinal;
-        this.isInitial = isInitial;
-    }
+    public State(String name) { this.name = name; }
 
     @Override
     public String toString() {
-        String final_state = "";
-        String init_state = "";
-        if (this.isInitial)
-            init_state = "Initial ";
-        if (this.isFinal)
-            final_state = "Final ";
 
-        return String.format("%s%sState %s", final_state, init_state, this.name);
+        return String.format("State %s", this.name);
     }
 }
