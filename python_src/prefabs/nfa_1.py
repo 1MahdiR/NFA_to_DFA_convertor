b041a87630d9206a16d07d389f294aceeb820096
m r <a.m.rasouli.n@gmail.com>
2021-12-20 14:39:27 +0330

    Fixed nfa_1 prefab

--- python_src/prefabs/nfa_1.py
+++ python_src/prefabs/nfa_1.py
@@ -11,7 +11,7 @@ q1 = State("q1")
 q2 = State("q2")
 q3 = State("q3")
 graph = { q0 : { "a" : [q1] , "b" : [q2] }, q1: { "a": [q3] },
-        q2 : { "b" : [q2, q3] }, q3 : {} }
+        q2 : { "b" : [q2, q3], "lambda" : [q3] }, q3 : {} }
 initial = q0
 finals = [q3]
 
