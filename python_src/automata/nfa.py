812609e0e7af96117e93b0fc85d7b28b41ced13e
m r <a.m.rasouli.n@gmail.com>
2021-12-20 20:41:27 +0330

    Changed list data types to frozensets, Added convertToDFA method

--- python_src/automata/nfa.py
+++ python_src/automata/nfa.py
@@ -8,10 +8,10 @@ class NFA:
             graph:dict, initialState:State, finalStates:list):
 
         self._name = name
-        self._alphabet = alphabet
+        self._alphabet = frozenset(alphabet)
         self._graph = graph
         self._initialState = initialState
-        self._finalStates = finalStates
+        self._finalStates = frozenset(finalStates)
 
     def recursiveFunction(self, w:str, currentState:State) -> bool:
         
@@ -43,7 +43,7 @@ class NFA:
     def doesAcceptString(self, w:str) -> bool:
         return self.recursiveFunction(w, self._initialState)
 
-    def nextState(self, st:State, w:str) -> list:
+    def nextState(self, st:State, w:str) -> frozenset:
 
         states = list()
 
@@ -63,18 +63,18 @@ class NFA:
                     for jtem in self.nextState(item, w):
                         states.append(jtem)
 
-        return list(set(states))
+        return frozenset(states)
 
-    def unionStates(self, states:list, w:str) -> list:
+    def unionStates(self, states:frozenset, w:str) -> list:
 
         union = list()
         for item in states:
             for jtem in self.nextState(item, w):
                 union.append(jtem)
 
-        return list(set(union))
+        return frozenset(union)
 
-    def createNewState(self, states:list):
+    def createNewState(self, states:frozenset):
         
         if not states:
             return State("empty_state")
@@ -83,5 +83,90 @@ class NFA:
         
         return State(state_name)
 
+    def convertToDFA(self):
 
+        graph = dict()
+
+        q0_state = frozenset([self._initialState])
+        q0_state = q0_state.union(self.nextState(self._initialState, "lambda"))
+
+        q0_edges = dict()
+        for i in self._alphabet:
+            q0_edges[i] = self.unionStates(q0_state, i)
+
+        graph[q0_state] = q0_edges
+        q0_state_dfa = None
+
+        graph_is_complete = False
+
+        while not graph_is_complete:
+
+            graph_is_complete = True
+            temp_graph = dict()
+            for item in graph.keys():
+                edges = graph[item]
+
+                for jtem in edges.values():
+                    if jtem not in graph:
+                        graph_is_complete = False
+
+                        current_edges = dict()
+
+                        for i in self._alphabet:
+                            current_edges[i] = self.unionStates(jtem, i)
+
+                        temp_graph[jtem] = current_edges
+
+
+
+            for item in temp_graph.keys():
+                graph[item] = temp_graph[item]
+
+        dfa_graph = dict()
+        finals = list()
+
+        mapper = dict()
+    
+        for item in graph.keys():
+            if item not in mapper.keys():
+                st = self.createNewState(item)
+
+                if item == q0_state:
+                    q0_state_dfa = st
+
+                for jtem in item:
+                    if jtem in self._finalStates:
+                        finals.append(st)
+                        break
+                
+                mapper[item] = st
+
+            else:
+                
+                st = mapper[item]
+                for jtem in item:
+                    if jtem in self._finalStates:
+                        finals.append(st)
+                        break
+
+            edges = dict()
+
+            for i in graph[item].keys():
+
+                states = graph[item][i]
+                if states not in mapper:
+                    new_st = self.createNewState(states)
+                    mapper[states] = new_st
+                    edges[i] = new_st
+                else:
+                    edges[i] = mapper[states]
+
+            dfa_graph[st] = edges
+
+        return DFA(self._name + "_(DFA)", self._alphabet, dfa_graph, q0_state_dfa, frozenset(finals))
+
+
+
+        
+        
 
