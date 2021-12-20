f9d71299b09c6794f2b4c5a474215dd76afca79b
m r <a.m.rasouli.n@gmail.com>
2021-12-20 09:09:02 +0330

    Change of plans! Restarted project with Python3, Added main.py file

---
+++ python_src/main.py
@@ -0,0 +1,15 @@
+
+# # # # # # # # # # # # # # # # # # # #
+# NFA to DFA convertor                #
+# author: __ray__                     #
+# email: a.m.rasouli.n@gmail.com      #
+# github: https://github.com/1MahdiR  #
+# # # # # # # # # # # # # # # # # # # #
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == '__main__':
+    main()
+
