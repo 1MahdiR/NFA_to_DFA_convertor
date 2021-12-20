05fd2425bc6f936d6f4b98ed2f91d1be25ee7bd9
m r <a.m.rasouli.n@gmail.com>
2021-12-20 09:58:04 +0330

    Added __repr__ method

--- python_src/automata/state.py
+++ python_src/automata/state.py
@@ -4,9 +4,12 @@ class State:
     def __init__(self, name:str):
         self._name = name
 
-    def getName(self):
+    def getName(self) -> str:
         return self._name
 
     def __str__(self):
         return "State %s" % self.getName()
+    
+    def __repr__(self):
+        return str(self)
 
